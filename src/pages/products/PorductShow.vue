2b94925663d861aaeaff866cd5713cc2edda2b07
Marcos Hernandez <maalhequi95@gmail.com>
2025-01-25 12:29:44 -0500

    style(Fix) enable similar product endpoint

--- src/pages/products/PorductShow.vue
+++ src/pages/products/PorductShow.vue
@@ -8,7 +8,7 @@
     <BannerMotowork
       :title="`${product.type === 'vehicle' ? 'Yamaha' : ''} ${product.name}`"
       :banner="productBanner || {}"
-      default-img=""
+      :default-img="product.type === 'vehicle' ? 'https://res.cloudinary.com/dg14xloef/image/upload/v1737398094/banners/boxfnfyssqzw809kxhsc.webp' : ''"
       :btnLabel="''"
       :bannerComplement="product.type === 'vehicle' ? 'Conquista cada camino con Yamaha: potencia, estilo y tecnología en una moto hecha para ti. Siente la libertad sobre dos ruedas con la fiabilidad y el rendimiento que solo Yamaha te puede ofrecer.' : product.description"
       :bannerAlt="`Banner de la pagina de la motocicleta ${product.name}, utilizado por Motowork en su Pagina Web`"
@@ -20,6 +20,12 @@
       <ShowProductData :product="product" />
     </section>
     <!--End items data-->
+
+    <!--Similar products-->
+    <section class="container-motowork bg-white">
+      <SimilarProducts />
+    </section>
+    <!--End similar products-->
   </q-page>
 </template>
 
@@ -32,6 +38,7 @@ import { notification } from 'src/boot/notification'
 import BreadCrumb from 'src/components/layout/BreadCrumb.vue'
 import BannerMotowork from 'src/components/banner/BannerMotowork.vue'
 import { useProductsContent } from 'src/composables/useProductContent'
+import SimilarProducts from 'src/components/products/partials/SimilarProducts.vue'
 import ShowProductData from 'src/components/products/partials/ShowProductData.vue'
 
 // references
