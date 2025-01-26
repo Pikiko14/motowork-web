6b8bf479ec8add36105fcb0d34a9907985a71d32
Marcos Hernandez <maalhequi95@gmail.com>
2025-01-26 12:18:37 -0500

    review function

--- src/pages/products/PorductShow.vue
+++ src/pages/products/PorductShow.vue
@@ -30,7 +30,7 @@
 <script setup>
 // imports
 import { computed } from 'vue'
-import { useQuasar } from 'quasar'
+import { useQuasar, useMeta } from 'quasar'
 import { useRoute, useRouter } from 'vue-router'
 import { notification } from 'src/boot/notification'
 import BreadCrumb from 'src/components/layout/BreadCrumb.vue'
@@ -65,6 +65,86 @@ const getProductData = async () => {
   const { reference } = route.query
   await showProduct(reference)
 }
+
+// set metadada
+const metaData = {
+  // sets document title
+  title: 'MotoWork',
+  // optional; sets final title as "Index Page - My Website", useful for multiple level meta
+  titleTemplate: title => `${title} - ${route.params.product_name}`,
+
+  // meta tags
+  meta: {
+    description: {
+      name: 'description',
+      content: 'Descrubre la información detallada de cada uno de nuestros productos y vive la mejor experiencia YAMAHA'
+    },
+    keywords: {
+      name: 'keywords',
+      content: 'Yamaha, motocicletas Yamaha, motos Yamaha, NMAX, CRYPTON, XMAX300, TMAX, MT-03, Ténéré 700, motos deportivas, motos urbanas, motos de aventura, motocicletas confiables, venta de motos, motos nuevas Yamaha, concesionarios Yamaha, motos económicas, accesorios Yamaha'
+    },
+    equiv: {
+      'http-equiv': 'Content-Type',
+      content: 'text/html; charset=UTF-8'
+    },
+    ogTitle: {
+      property: 'og:title',
+      template (ogTitle) {
+        return `${ogTitle} - Listado de ${route.query.type === 'vehicle' ? 'Motocicletas' : 'Accesorios'}`
+      }
+    },
+    ogDescription: {
+      property: 'og:description',
+      content: 'Descubre nuestra amplia gama de motocicletas Yamaha. ¡La moto perfecta para cada aventura está aquí!'
+    },
+    ogType: {
+      property: 'og:type',
+      content: 'website'
+    },
+    ogUrl: {
+      property: 'og:url',
+      content: 'https://www.motowork.co'
+    },
+    twitterCard: {
+      name: 'twitter:card',
+      content: 'summary_large_image'
+    },
+    twitterTitle: {
+      name: 'twitter:title',
+      content: 'MotoWork - Web'
+    },
+    twitterDescription: {
+      name: 'twitter:description',
+      content: 'Explora las mejores motocicletas Yamaha: diseño, innovación y aventura en cada modelo.'
+    },
+    robots: {
+      name: 'robots',
+      content: 'index, follow'
+    },
+    language: {
+      name: 'language',
+      content: 'es'
+    }
+  },
+
+  // CSS tags
+  link: {
+    favicon: {
+      rel: 'icon',
+      href: '/favicon.ico'
+    },
+    canonical: {
+      rel: 'canonical',
+      href: 'https://www.motowork.co'
+    }
+  },
+
+  // <noscript> tags
+  noscript: {
+    default: 'Este contenido es visible para navegadores sin soporte de JavaScript o con JavaScript desactivado.'
+  }
+}
+useMeta(metaData)
 
 // hook
 if (!route.query.reference) {
